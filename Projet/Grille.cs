b7abb0daf6bd45d30a828b70f940787c5d927530
Lucas Sainte-croix <lucas.stx@mailo.com>
2025-04-14 14:44:02 +0200

    addingsss

--- Projet/Grille.cs
+++ Projet/Grille.cs
@@ -18,4 +18,29 @@ public class Grille
             }
         }
     }
+     // Afficher la première ligne de séparateurs
+    for (int i = 0; i < plateau.GetLength(1); i++)
+    {
+        Console.Write("╬═══"); // chaque cellule est encadrée de " ╬═══ "
+    }
+    Console.WriteLine("╬");
+
+    // Affichage des lignes du plateau
+    for (int i = 0; i < plateau.GetLength(0); i++)
+    {
+        // Affichage des cases de chaque ligne
+        for (int j = 0; j < plateau.GetLength(1); j++)
+        {
+            Console.Write("║ " + plateau[i, j] + " "); // Chaque case est entourée de " ║ "
+        }
+        Console.WriteLine("║");
+
+        // Affichage de la ligne de séparation après chaque ligne de cases
+        for (int j = 0; j < plateau.GetLength(1); j++)
+        {
+            Console.Write("╬═══"); // Séparateur après chaque ligne de cases
+        }
+        Console.WriteLine("╬");
+    }
+}
 }
--- Projet/Plante.cs
+++ Projet/Plante.cs
@@ -1,6 +1,14 @@
 public abstract class Plante
 {
-    public bool comestible;
-    public string saison;
+    public bool miam { get; set; };
+    //nature de la plante : si elle est bonne à manger ou pas
+    public string saison { get; set; };
     //public Terrain terrain;
+    public int espacement { get; set; }
+    public int place { get; set; }
+    public int vitesse { get; set; }
+    // faire un dictionnaire pour les maladies TODO
+    public int espéranceVie { get; set; }
+    publicc int nbFruits { get; set; }
+    public Plante(bool Miam, )
 }
