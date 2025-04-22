e002abcb4156f0e60558e5bcb610a311585ff3ff
angallou <angallou@ensc.fr>
2025-04-22 12:19:13 +0200

    Mise à jour saison

--- Projet/Program.cs
+++ Projet/Program.cs
@@ -2,5 +2,8 @@
 Grille grille = new Grille();
 Console.Clear();
 grille.Afficher();
-Hiver hiver = new Hiver();
+Saison hiver = new Saison(0.8,0.2);
+Saison printemps = new Saison(0.3,0.7);
+Saison ete = new Saison(0.1,0.9);
+Saison automne = new Saison(0.6,0.4);
 hiver.CalculerMeteo();
--- Projet/Saison.cs
+++ Projet/Saison.cs
@@ -1,11 +1,30 @@
 public abstract class Saison
 {
+    Random rng = new Random();
     public double Pluie;
     public double Soleil;
-    public Saison (double pluie, double soleil)
+    public Saison(double pluie, double soleil)
     {
         Pluie = pluie;
         Soleil = soleil;
     }
-    public string CalculerMeteo();
+    public abstract string CalculerMeteo()
+    {
+        double pluie = rng.Next(0,1);
+        double soleil = rng.Next(0,1);
+        if (pluie > Pluie)
+        {
+            if (soleil > Soleil)
+                return "Nuageux";
+            else
+                return "Ensoleillé";
+        }
+        else
+        {
+            if (soleil > Soleil)
+                return "Pluvieux";
+            else
+                return "Averse ensoleillée";
+        }
+    };
 }
