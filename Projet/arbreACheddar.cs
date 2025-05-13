60a2a56aabc5b3875884c073a21fcebffaba0caa
Turbul-e <lucas.stx@mailo.com>
2025-05-13 17:30:13 +0200

    bug sur les toxinelles à régler

--- Projet/arbreACheddar.cs
+++ Projet/arbreACheddar.cs
@@ -1,6 +1,6 @@
 public class ArbreACheddar : Plante
 {
-    public ArbreACheddar(int X, int Y) : base("Arbre à Cheddar",X, Y, true, true, "Automne", "Fromage", 1, 3, 4, 0.6, 0.5, 10, 25, "Toxinelle", 6, 4)
+    public ArbreACheddar(int X, int Y) : base("Arbre à Cheddar",X, Y, true, true, Automne, "Fromage", 1, 3, 4, 0.6, 0.5, 10, 25, Toxinelles, 6, 4)
     {
 
     }
