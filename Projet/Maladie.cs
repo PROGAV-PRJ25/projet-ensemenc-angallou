1d8af38ab3769ef3265509d980e918bebba2aea9
Turbul-e <lucas.stx@mailo.com>
2025-05-13 18:17:27 +0200

    Ajout TypeOf pour les maladies

--- Projet/Maladie.cs
+++ Projet/Maladie.cs
@@ -1,27 +1,41 @@
-﻿public abstract class Maladie {
-    public string nom {get; set;}
+﻿public abstract class Maladie
+{
     public int x { get; set; }
     public int y { get; set; }
-    public Plante planteCible {get; set;}
-    public double attaque {get; set;}
-    public string faiblesse {get; set;} 
-    // [] -> à voir si on en met qu'une de faiblesse ou pas
-    public bool modeUrgence {get; set;}
-public Maladie(int X, int Y, bool ModeUrgence, Plante PlanteCible, double Attaque, string Faiblesse)
- {
-x = X ;
-y = Y;
-planteCible = PlanteCible;
-attaque = Attaque;
-faiblesse=Faiblesse;
-modeUrgence = ModeUrgence;
-}
-public abstract class Invasion{
+    public bool modeUrgence { get; set; }
+    public double attaque { get; set; }
+    public string faiblesse { get; set; }
+    public Type typePlanteCible { get; set; } // Type de plante visée par la maladie
 
+    public Maladie(
+        int X,
+        int Y,
+        bool ModeUrgence,
+        double Attaque,
+        string Faiblesse,
+        Type TypePlanteCible
+    )
+    {
+        x = X;
+        y = Y;
+        modeUrgence = ModeUrgence;
+        attaque = Attaque;
+        faiblesse = Faiblesse;
+        typePlanteCible = TypePlanteCible;
+    }
+
+    public virtual bool EstPlanteCible(Plante plante)
+    {
+        return plante.GetType() == typePlanteCible;
+    }
+
+    public class Infecter() { } //TODO
 }
-}
+
 //gérer le mode urgence
 //gérer les différentes maladies
 // gérer les différentes attaques
 //est-ce que je met un bool il y a plante ? mehhh on verra ça dans les codes au cas par cas
 // Ne pas oublier de faire le mode urgence et les différentes manières de se protéger des invasions
+
+// faiblesse TODO
--- Projet/arbreACheddar.cs
+++ Projet/arbreACheddar.cs
@@ -1,16 +1,7 @@
 public class ArbreACheddar : Plante
 {
-<<<<<<< HEAD
     public ArbreACheddar(int X, int Y) : base("Arbre à Cheddar",X, Y, true, true, Automne, "Fromage", 1, 3, 4, 0.6, 0.5, 10, 25, Toxinelles, 6, 4)
-=======
-<<<<<<< HEAD
-    public ArbreACheddar(int X, int Y) : base(X, Y, true, true, Automne, Fromage, 1, 3, 4, 0.6, 0.5, 10, 25, Toxinelle, 6, 4)
-    {}
-=======
-    public ArbreACheddar(int X, int Y) : base("Arbre à Cheddar",X, Y, true, true, "Automne", "Fromage", 1, 3, 4, 0.6, 0.5, 10, 25, "Toxinelle", 6, 4)
->>>>>>> 4c522e25e2930b50360e27087be9cd662ac8a774
     {
 
     }
->>>>>>> 8b9991ec94cfa56e89a24293b07ff69356d9577d
 }
