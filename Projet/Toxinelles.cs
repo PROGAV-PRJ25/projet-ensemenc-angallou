8507d9c6d53489d5f6fdecb004897c985ae1be8a
angallou <angallou@ensc.fr>
2025-05-13 18:21:32 +0200

    Modifs

--- Projet/Toxinelles.cs
+++ Projet/Toxinelles.cs
@@ -1,6 +1,5 @@
 public class Toxinelles : Maladie
 {
-<<<<<<< HEAD
 public Toxinelles (int X, int Y) : base (X, Y, false, ArbreACheddar hihi, 100, "Plantes Carnivores") { }
 =======
     public Toxinelles(int X, int Y)
@@ -10,10 +9,4 @@ public Toxinelles (int X, int Y) : base (X, Y, false, ArbreACheddar hihi, 100, "
     {
         return PlanteCible is ArbreACheddar;
     }
->>>>>>> 1d8af38ab3769ef3265509d980e918bebba2aea9
 }
-//if plante.x && plante.y == maladie (x, y)
-// & if plante.nom == plantecible
-// then full attack TODO
-// if defense.x && defense.y == maladie(x, y) (avec une marge si plante à proximité)
-// then remove menace
