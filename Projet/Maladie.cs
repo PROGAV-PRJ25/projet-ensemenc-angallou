c0c81fa3f505c93e69be39ab9b2dc9c7f3135e9e
Turbul-e <lucas.stx@mailo.com>
2025-05-13 16:10:35 +0200

    avancement maladies

--- Projet/Maladie.cs
+++ Projet/Maladie.cs
@@ -1,1 +1,15 @@
-﻿
+﻿public abstract class Maladie {
+    public int x { get; set; }
+    public int y { get; set; }
+public Plante plante{get; set;}
+public Saison saison {get; set;}
+public Maladie {
+
+}
+public abstract class Envahir{
+
+}
+}
+//gérer le mode urgence
+//gérer les différentes maladies
+// gérer les différentes attaques
